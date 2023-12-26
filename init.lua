fa17a8f9122eb5eb115223feb23162385603d558
Claes Persson <c@cp.se>
2023-12-26 22:06:14 +0100

    Reverting to a single file. For now.

--- init.lua
+++ init.lua
@@ -1,3 +1,32 @@
+vim.g.mapleader = " "
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.wrap = false
+
+vim.opt.incsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.termguicolors = true
+vim.opt.colorcolumn = "80"
+vim.opt.signcolumn = "yes"
+vim.opt.cmdheight = 1
+vim.opt.scrolloff = 10
+
+vim.opt.errorbells = false
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undofile = true
+vim.opt.backspace = "indent,eol,start"
+vim.opt.iskeyword:append("-")
+vim.opt.mouse:append("a")
+vim.opt.clipboard:append("unnamedplus")
+
 -- Lazy.vim installation
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
@@ -12,5 +41,51 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("vim-options")
-require("lazy").setup("plugins")
+local opts = {}
+local plugins = {
+    { "catppuccin/nvim", name="catppuccin", priority = 1000 },
+    { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+    { "nvim-telescope/telescope.nvim", tag = "0.1.5", dependencies = { "nvim-lua/plenary.nvim" } },
+    { "nvim-telescope/telescope-ui-select.nvim" },
+    { "nvim-neo-tree/neo-tree.nvim", branch = "v3.x", dependencies = { "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons", "MunifTanjim/nui.nvim" } },
+    { "williamboman/mason.nvim" },
+    { "williamboman/mason-lspconfig.nvim" },
+    { "neovim/nvim-lspconfig" },
+    { "nvim-lualine/lualine.nvim", dependencies = { "nvim-tree/nvim-web-devicons" } }
+}
+require("lazy").setup(plugins, opts)
+
+require("catppuccin").setup({})
+vim.cmd.colorscheme "catppuccin-mocha"
+
+require("nvim-treesitter.configs").setup({
+    ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "rust", "html" },
+    sync_install = false,
+    highlight = { enable = true },
+    indent = { enable = true },
+})
+
+local telescope_builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", telescope_builtin.find_files, {})
+vim.keymap.set("n", "<leader>fg", telescope_builtin.live_grep, {})
+vim.keymap.set("n", "<leader>fb", telescope_builtin.buffers, {})
+vim.keymap.set("n", "<leader>fh", telescope_builtin.help_tags, {})
+
+require("telescope").load_extension("ui-select")
+require("neo-tree").setup({
+    close_if_last_window = true
+})
+vim.keymap.set("n", "<C-l>", ":Neotree toggle<CR>", { silent = true })
+
+require("mason").setup({})
+require("mason-lspconfig").setup({
+    ensure_installed = { "lua_ls", "rust_analyzer" },
+})
+require("lspconfig").tsserver.setup({})
+require("lspconfig").rust_analyzer.setup({})
+
+require("lualine").setup({
+    options = {
+        theme = 'auto'
+    }
+})

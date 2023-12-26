6715eabbb1b9f823125c241181498fc55101cc2e
Claes Persson <c@cp.se>
2023-12-26 22:29:07 +0100

    Keymaps for LSP

--- init.lua
+++ init.lua
@@ -81,8 +81,30 @@ require("mason").setup({})
 require("mason-lspconfig").setup({
     ensure_installed = { "lua_ls", "rust_analyzer" },
 })
-require("lspconfig").tsserver.setup({})
-require("lspconfig").rust_analyzer.setup({})
+local lspconfig = require("lspconfig")
+lspconfig.tsserver.setup({})
+lspconfig.rust_analyzer.setup({})
+
+vim.api.nvim_create_autocmd("LspAttach", {
+    group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+    callback = function(ev)
+        vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+
+        local opts = { buffer = ev.buf }
+        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+        vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+        vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts)
+        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+        vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+        vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+        vim.keymap.set("n", "<leader>f", function()
+            vim.lsp.buf.format { async = true }
+        end, opts)
+    end,
+})
 
 require("lualine").setup({
     options = {

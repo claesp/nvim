822fd94946823cb6b88c5fb0db663893ea78cd34
Claes Persson <c@cp.se>
2023-12-26 16:51:18 +0100

    Additional Vim settings.

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,6 +1,29 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=4")
-vim.cmd("set softtabstop=4")
-vim.cmd("set shiftwidth=4")
 vim.g.mapleader = " "
 
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.wrap = false
+
+vim.opt.incsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.termguicolors = true
+vim.opt.colorcolumn = "80"
+vim.opt.signcolumn = "yes"
+vim.opt.cmdheight = 1
+vim.opt.scrolloff = 10
+
+vim.opt.errorbells = false
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undofile = true
+vim.opt.backspace = "indent,eol,start"
+vim.opt.iskeyword:append("-")
+vim.opt.mouse:append("a")
+vim.opt.clipboard:append("unnamedplus")
